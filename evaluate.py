5afbbd73134fac9c373d303928312bbc71a6afa9
Benjamin Houghton <bth51@georgetown.edu>
2022-04-21 16:54:41 -0400

    Update evaluate.py and lstm model, using OHE response variables

--- evaluate.py
+++ evaluate.py
@@ -1,10 +1,11 @@
 # imports
 import argparse
 import numpy as np
+from pathlib import Path
 from gensim.models.word2vec import Word2Vec
 
 
-def main(w2v_model_file, yp_vectors, y_vectors):
+def main(w2v_model_file, ohe_vectors, predicted_vectors, token_arr):
 
     # load the Word2Vec model
     w2v_mod = Word2Vec.load(w2v_model_file)
@@ -12,50 +13,77 @@ def main(w2v_model_file, yp_vectors, y_vectors):
     # forms the KeyedVectors token --> vector lookup dictionary
     w2v_vecs = w2v_mod.wv
 
-    # load the predicted and true vectors
-    testYp = np.load(yp_vectors)
-    testY = np.load(y_vectors)
-    trainYp = np.load('data/ohe_vectors/trainYp.npy')
-    trainY = np.load('data/ohe_vectors/trainY.npy')
+    # Define path to the ohe and predicted vectors
+    ohe_dir = Path(__file__).parent.joinpath(ohe_vectors)
+    pre_dir = Path(__file__).parent.joinpath(predicted_vectors)
 
-    # Get most common words for predicted and actual words
-    # print(w2v_vecs.similar_by_vector(testYp[50]))
-    # print(w2v_vecs.similar_by_vector(testY[50]))
-    # print(w2v_vecs.similar_by_vector(trainYp[100]))
-    # print(w2v_vecs.similar_by_vector(trainY[100]))
+    # Load the token array
+    tok_arr = np.load(token_arr)
 
-    testA = []
-    testP = []
-    testC = 0
-    trainA = []
-    trainP = []
-    trainC = 0
+    # Define dictionary to store vectors
+    vectors = {
+        'testY': None,
+        'model1': None,
+        'model2': None,
+        'model3': None
+    }
 
-    for vp, va in zip(testYp, testY):
+    # Iterate over the files in the ohe directory
+    for f in ohe_dir.iterdir():
 
-        testa = w2v_vecs.similar_by_vector(va)[0][0]
-        testA.append(testa)
+        # Check if the file name is desired dictionary key list
+        if f.name.replace('.npy', '') in vectors.keys():
 
-        testp = [word[0] for word in w2v_vecs.similar_by_vector(vp)]
-        testP.append(testp)
+            # Load the vector
+            vectors[f.name.replace('.npy', '')] = np.load(f)
 
-        if testa in testp:
-            testC += 1
+    # Iterate over the files in the predictions directory
+    for f in pre_dir.iterdir():
 
-    print('The correct word was in testing predictions {} out of {} records'.format(testC, len(testA)))
+        # Check if the file name is desired dictionary key list
+        if f.name.replace('.npy', '') in vectors.keys():
 
-    for vp, va in zip(trainYp, trainY):
+            # Load the vector
+            vectors[f.name.replace('.npy', '')] = np.load(f)
 
-        traina = w2v_vecs.similar_by_vector(va)[0][0]
-        trainA.append(traina)
+    # Define the actual word idx and actual word
+    act_idx = np.argmax(vectors['testY'], axis=1)
 
-        trainp = [word[0] for word in w2v_vecs.similar_by_vector(vp)]
-        trainP.append(trainp)
+    # Define the actual word array
+    act_word = tok_arr[act_idx]
 
-        if traina in trainp:
-            trainC += 1
+    # Get the top 9 most similar words for the actual word and add the actual
+    # word into 2 dimensional list
+    top_ten = [
+        [sim_word[0] for sim_word in w2v_vecs.similar_by_key(word, topn=9)] + [word]
+        for word in list(act_word)
+    ]
 
-    print('The correct word was in training predictions {} out of {} records'.format(trainC, len(trainA)))
+    # Iterate over the vectors
+    for name in vectors.keys():
+
+        # check if the vector is a prediction or actual vector
+        if vectors[name] is not None and name != 'testY':
+
+            # Define the predicted word index and actual word
+            pre_idx = np.argmax(vectors[name], axis=1)
+            pre_word = tok_arr[pre_idx]
+            records = act_idx.shape[0]
+            correct = np.where(act_idx == pre_idx, 1, 0).sum()
+            acc = correct/records
+            print('The test accuracy of model {} is {:.3f}%'.format(name[-1], acc*100))
+
+            # Begin a predicted in top ten counter
+            success = 0
+
+            # Iterate over every top ten record set and prediction
+            for word, sim_words in zip(pre_word, top_ten):
+
+                # Check if the prediction is in the top 10 most similar words
+                if word in sim_words:
+                    success += 1
+
+            print('The number of times the predicted word was in the top 10 most similar words was {}'.format(success))
 
 
 if __name__ == "__main__":
@@ -64,14 +92,22 @@ if __name__ == "__main__":
     parser.add_argument("--w2v_model_file", type=str,
                         default="models/word2vec.model",
                         help="The trained word2vec model file path")
-    parser.add_argument("--yp_vectors", type=str,
-                        default="data/ohe_vectors/testYp.npy",
-                        help="The predicted word vectors")
-    parser.add_argument("--y_vectors", type=str,
-                        default="data/ohe_vectors/testY.npy",
-                        help="The true word vectors")
+    parser.add_argument("--ohe_vectors", type=str,
+                        default="data/ohe_vectors",
+                        help="The ohe word vectors")
+    parser.add_argument("--predicted_vectors", type=str,
+                        default="data/predicted",
+                        help="The predicted ohe vectors")
+    parser.add_argument("--token_arr", type=str,
+                        default="data/tok_arr.npy",
+                        help="The token index array file path")
 
     args = parser.parse_args()
 
-    main(args.w2v_model_file, args.yp_vectors, args.y_vectors)
+    main(
+        args.w2v_model_file,
+        args.ohe_vectors,
+        args.predicted_vectors,
+        args.token_arr
+    )
 
--- train_lstm_model1.py
+++ train_lstm_model1.py
@@ -6,13 +6,13 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 from keras import regularizers
 from keras.models import Sequential
-from keras.layers import LSTM, Dense, Embedding, Dropout, Conv1D, MaxPooling1D, GlobalMaxPooling1D
+from keras.layers import LSTM, Bidirectional, Dense, Embedding, Dropout
 from keras.callbacks import ModelCheckpoint
-
 
 
 def main(
         vector_directory,
+        ohe_directory,
         epochs,
         units,
         activation,
@@ -21,74 +21,115 @@ def main(
         input_shape,
         model_file
 ):
+    """
+    Trains a bidirectional LSTM model using previous words as predictor
+    variables and the next word as the response variable. Both input and output
+    words are represented as 100 dimensional word2vec vectors.
+
+    Args:
+        vector_directory (str):
+        ohe_directory (str)
+        epochs (int):
+        units (int):
+        activation (str):
+        recurrent_activation (str):
+        dense_activation (str):
+        input_shape (tuple):
+        model_file (str):
+
+    Returns:
+        None
+
+    Raises:
+        None
+
+    """
 
     # Instantiate Path object for word vector directory
     v_dir = Path(__file__).parent.joinpath(vector_directory)
+    ohe_dir = Path(__file__).parent.joinpath(ohe_directory)
 
     # Instantiate dictionary to store the word vector arrays
     vectors = {}
 
+    # Iterate over the word2vec vector files
     for file in v_dir.iterdir():
 
-        print('Loading {}'.format(file.name))
+        # Check if vector is predictor variable
+        if 'X' in file.name:
+            print('Loading {}'.format(file.name))
 
-        # Define the key for the array
-        key = file.name.replace('.npy', '')
+            # Define the key for the array
+            key = file.name.replace('.npy', '')
 
-        # Load the word vector array and place into dictionary
-        vectors[key] = np.load(str(file))
+            # Load the word vector array and place into dictionary
+            vectors[key] = np.load(str(file))
 
-        print('The vector shape is {}\n'.format(vectors[key].shape))
+            print('The vector shape is {}\n'.format(vectors[key].shape))
+
+    # Iterate over the ohe vector files
+    for file in ohe_dir.iterdir():
+
+        if 'Y' in file.name:
+            print('Loading {}'.format(file.name))
+
+            # Define the key for the array
+            key = file.name.replace('.npy', '')
+
+            # Load the word vector array and place into dictionary
+            vectors[key] = np.load(str(file))
+
+            print('The vector shape is {}\n'.format(vectors[key].shape))
+
+    # Define the vocab size
+    vocab_size = vectors[key].shape[1]
 
     # Check if gpu available
     if tf.config.list_physical_devices('GPU'):
         # Define message for logger
         msg = '##### Using GPU for training #####'
-        # Log message
+        # Log/print message
         #logger.info(msg)
         print(msg)
 
     # Specify sequential model stack
     model = Sequential()
 
+    print(input_shape)
+
     # Add LSTM layer
     model.add(
-        LSTM(
-            units=units,
-            activation=activation,
-            recurrent_activation=recurrent_activation,
-            #kernel_regularizer=regularizers.l1_l2(0.001, 0.001),
-            #recurrent_regularizer=regularizers.l1_l2(0.001, 0.001),
-            #bias_regularizer=regularizers.l1_l2(0.001, 0.001),
-            #dropout=P['DO'],
-            #recurrent_dropout=P['RD'],
-            return_sequences=True,
+        Bidirectional(
+            LSTM(
+                units=units,
+                activation=activation,
+                recurrent_activation=recurrent_activation,
+                #kernel_regularizer=regularizers.l1_l2(0.001, 0.001),
+                #recurrent_regularizer=regularizers.l1_l2(0.001, 0.001),
+                #bias_regularizer=regularizers.l1_l2(0.001, 0.001),
+                #dropout=P['DO'],
+                #recurrent_dropout=P['RD'],
+                return_sequences=True
+            ),
             input_shape=input_shape
         )
     )
 
     # Add LSTM layer
     model.add(
-        LSTM(
-            units=units*2,
-            activation=activation,
-            recurrent_activation=recurrent_activation,
-            #kernel_regularizer=regularizers.l1_l2(0.001, 0.001),
-            #recurrent_regularizer=regularizers.l1_l2(0.001, 0.001),
-            #bias_regularizer=regularizers.l1_l2(0.001, 0.001),
-            #dropout=P['DO'],
-            #recurrent_dropout=P['RD'],
-            # return_sequences=True,
-            input_shape=input_shape
-        )
-    )
-    # Add dense layer
-    model.add(
-        Dense(
-            units=1000,
-            activation=dense_activation,
-            #kernel_regularizer=regularizers.l1_l2(P['L1'], P['L2']),
-            #bias_regularizer=regularizers.l1_l2(P['L1'], P['L2']),
+        Bidirectional(
+            LSTM(
+                units=units*2,
+                activation=activation,
+                recurrent_activation=recurrent_activation,
+                #kernel_regularizer=regularizers.l1_l2(0.001, 0.001),
+                #recurrent_regularizer=regularizers.l1_l2(0.001, 0.001),
+                #bias_regularizer=regularizers.l1_l2(0.001, 0.001),
+                #dropout=P['DO'],
+                #recurrent_dropout=P['RD'],
+                #return_sequences=True
+                #input_shape=input_shape
+            )
         )
     )
 
@@ -96,7 +137,17 @@ def main(
     model.add(
         Dense(
             units=100,
-            activation=dense_activation,
+            activation='relu'
+            #kernel_regularizer=regularizers.l1_l2(P['L1'], P['L2']),
+            #bias_regularizer=regularizers.l1_l2(P['L1'], P['L2']),
+        )
+    )
+
+    # Add the final dense layer (outputs probability for each word in vocab)
+    model.add(
+        Dense(
+            units=9999,
+            activation=dense_activation
             #kernel_regularizer=regularizers.l1_l2(P['L1'], P['L2']),
             #bias_regularizer=regularizers.l1_l2(P['L1'], P['L2']),
         )
@@ -108,14 +159,15 @@ def main(
     # Compile the model
     model.compile(
         optimizer='rmsprop',
-        loss='mse'
+        metrics=['acc'],
+        loss='categorical_crossentropy'
     )
 
     # Define callback to save best model based upon validation mae
     model_checkpoint = ModelCheckpoint(
         filepath=model_file,
         save_weights_only=False,
-        monitor='val_loss',
+        monitor='val_acc',
         mode='auto',
         save_best_only=True
     )
@@ -130,32 +182,44 @@ def main(
         callbacks=[model_checkpoint]
     )
 
-    # Get the mse and acc for training and validation data
+    # Get the mse and mae for training data and validaiton data
     loss = history.history['loss']
     val_loss = history.history['val_loss']
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
 
     # Define the epoch range
     epochs = range(1, len(loss) + 1)
 
     # Define a plot figure with 4 subplots
-    fig, ax = plt.subplots(figsize=(8, 6), dpi=100)
+    fig, ax = plt.subplots(figsize=(8, 8), nrows=2, ncols=1, dpi=300)
 
-    # Define the mae plot
-    ax.plot(epochs, loss, 'bo', label='Training Loss')
-    ax.plot(epochs, val_loss, 'b', label='Validation Loss')
-    ax.set_title('Training and Validation Loss')
-    ax.set_xlabel('Epoch Number')
-    ax.set_ylabel('Loss (Mean Squared Error)')
-    ax.legend()
+    # Adjust the plot spacing
+    plt.subplots_adjust(hspace=0.3, wspace=0.3)
+
+    # Define the loss plot
+    ax[0].plot(epochs, loss, 'bo', label='Training Loss')
+    ax[0].plot(epochs, val_loss, 'b', label='Validation Loss')
+    ax[0].set_title('Training and Validation Loss')
+    ax[0].set_xlabel('Epoch Number')
+    ax[0].set_ylabel('Loss (Categorical Cross-Entropy)')
+    ax[0].legend()
+
+    # Define the loss plot
+    ax[1].plot(epochs, acc, 'bo', label='Training ACC')
+    ax[1].plot(epochs, val_acc, 'b', label='Validation ACC')
+    ax[1].set_title('Training and Validation ACC')
+    ax[1].set_xlabel('Epoch Number')
+    ax[1].set_ylabel('Accuracy')
+    ax[1].legend()
 
     # Save and show the plot
     plt.savefig('plots/loss1.png')
     #plt.show()
 
+    # Predict the test data and write to file
     testYp = model.predict(vectors['testX'])
-    trainYp = model.predict(vectors['trainX'])
-    np.save('data/vectors/testYp.npy', testYp)
-    np.save('data/vectors/trainYp.npy', trainYp)
+    np.save('data/predicted/model1.npy', testYp)
 
 
 if __name__ == "__main__":
@@ -167,6 +231,12 @@ if __name__ == "__main__":
         type=str,
         default="data/vectors",
         help="word vector directory relative file path"
+    )
+    parser.add_argument(
+        "--ohe_directory",
+        type=str,
+        default="data/ohe_vectors",
+        help="ohe vector directory relative file path"
     )
     parser.add_argument(
         "--epochs",
@@ -183,19 +253,19 @@ if __name__ == "__main__":
     parser.add_argument(
         "--activation",
         type=str,
-        default='relu',
+        default='tanh',
         help="The LSTM activation function"
     )
     parser.add_argument(
         "--recurrent_activation",
         type=str,
-        default='tanh',
+        default='sigmoid',
         help="The LSTM recurrent activation function"
     )
     parser.add_argument(
         "--dense_activation",
         type=str,
-        default='relu',
+        default='softmax',
         help="The LSTM recurrent activation function"
     )
     parser.add_argument(
@@ -207,13 +277,14 @@ if __name__ == "__main__":
     parser.add_argument(
         "--model_file",
         type=str,
-        default='models/LSTM.h5',
+        default='models/LSTM1.h5',
         help="Path to save model"
     )
     args = parser.parse_args()
 
     main(
         args.vector_directory,
+        args.ohe_directory,
         args.epochs,
         args.units,
         args.activation,
